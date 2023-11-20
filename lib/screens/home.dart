0ce81ab2596e0fb81bbb9905c6a9705fe78ce4df
Salim Ali <u14262208@tuks.co.za>
2023-11-20 12:17:46 +0200

    added custom fonts

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,7 +9,8 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(backgroundColor: granite);
+    return Scaffold(appBar: AppBar(elevation: 0, backgroundColor: Colors.transparent, foregroundColor: Colors.white), backgroundColor: granite);
+    // return Scaffold(appBar: AppBar(elevation: 0, backgroundColor: Colors.transparent, surfaceTintColor: Colors.white), backgroundColor: granite);
 
   }
 
