c4c6dbf136924e4bac1419fa053148c87a2b9c32
Salim Ali <u14262208@tuks.co.za>
2023-11-20 11:45:34 +0200

    set up project structure

---
+++ lib/screens/home.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+import "package:neutrino/utility/constants.dart";
+
+
+class Home extends StatelessWidget {
+
+  const Home({ super.key });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(backgroundColor: granite);
+
+  }
+
+}
