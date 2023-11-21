92952d92bf10137a0eafd6ca48aa573d88e78875
Salim Ali <u14262208@tuks.co.za>
2023-11-21 06:48:46 +0200

    improved design

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,17 +11,18 @@ class Home extends StatelessWidget {
 
     final screenWidth = MediaQuery.of(context).size.width;
 
-    // return Scaffold(appBar: AppBar(elevation: 0, backgroundColor: Colors.transparent, surfaceTintColor: Colors.white), backgroundColor: granite);
     return Scaffold(appBar: AppBar(elevation: 0, backgroundColor: Colors.transparent, foregroundColor: Colors.white), backgroundColor: granite,
+
                     body: SingleChildScrollView(physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-                                                child: Column(children: [Container(margin: EdgeInsets.only(left: screenWidth*0.05), child: Column(crossAxisAlignment: CrossAxisAlignment.start, 
+
+                                                child: Column(children: [Row(mainAxisAlignment: MainAxisAlignment.center, children: [Container(width: screenWidth*0.9, child: Column(crossAxisAlignment: CrossAxisAlignment.start, 
                                                                                                  children: [Text("Welcome,", style: TextStyle(color: Colors.white, fontFamily: "Futura",
                                                                                                                                               fontWeight: FontWeight.w500, fontSize: 16)),
                                                                                                             
                                                                                                             Row(crossAxisAlignment: CrossAxisAlignment.start, children: [Text("Salim".toUpperCase(), style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700,
                                                                                                                                                               fontFamily: "Getai", fontSize: 32)),
                                                                                                                                                               
-                                                                                                                           Image.asset("assets/icons/flame.png", width: 36)])]))])));
+                                                                                                                           Image.asset("assets/icons/flame.png", width: 36)])]))])])));
 
   }
 
