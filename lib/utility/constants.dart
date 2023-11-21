c2930cc044abc8b35e4b35e0c616469a656fd998
Salim Ali <u14262208@tuks.co.za>
2023-11-21 07:43:35 +0200

    created the card

--- lib/utility/constants.dart
+++ lib/utility/constants.dart
@@ -2,3 +2,10 @@ import "package:flutter/material.dart";
 
 
 const Color granite = Color(0xff1e1e1e);
+// const Color blue = Color(0xff6953f7);
+// const Color sky = Color(0xff87cee6);
+const Color ocean = Color(0xff00bfff);
+const Color purple = Color(0xffcd4ff7);
+const Color green = Color(0xff86dc3d);
+const Color pink = Color(0xffff6ec7);
+const Color orange = Color(0xfff28500);
--- lib/widgets/info_card.dart
+++ lib/widgets/info_card.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:neutrino/utility/constants.dart";
 
 
 class InfoCard extends StatelessWidget {
@@ -8,7 +9,10 @@ class InfoCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Container();
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Container(height: 160, width: screenWidth*0.48,
+                     decoration: BoxDecoration(color: ocean, borderRadius: BorderRadius.all(Radius.circular(10))));
 
   }
 
