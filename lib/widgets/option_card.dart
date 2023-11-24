a7e881acb80bf35bd59783268994a6bdf598edcd
Salim Ali <u14262208@tuks.co.za>
2023-11-24 08:56:58 +0200

    made the option card pressable

--- lib/widgets/option_card.dart
+++ lib/widgets/option_card.dart
@@ -13,12 +13,13 @@ class OptionCard extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
 
     // return Container(width: screenWidth*0.9, height: 100, padding: const EdgeInsets.all(10),
-    return Center(child: Container(width: screenWidth*0.9, height: 80, padding: const EdgeInsets.all(10),
-                     decoration: BoxDecoration(color: blue, borderRadius: BorderRadius.all(Radius.circular(5))),
-                    //  decoration: BoxDecoration(color: nectarine, borderRadius: BorderRadius.all(Radius.circular(10))),
-                     child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [Image.asset("assets/icons/meal.png"), Text("Meal plans", style: TextStyle(color: Colors.white, fontFamily: "Futura", fontWeight: FontWeight.w700, fontSize: 18))])));
-                                // children: [Image.asset("assets/icons/meal.png"), Text("Meal plan", style: TextStyle(color: granite, fontFamily: "Futura", fontWeight: FontWeight.w500, fontSize: 18))]));
+    return GestureDetector(onTap: () => print("option card"), 
+                           child: Center(child: Container(width: screenWidth*0.9, height: 80, padding: const EdgeInsets.all(10),
+                                                          decoration: BoxDecoration(color: blue, borderRadius: BorderRadius.all(Radius.circular(5))),
+                                                          //  decoration: BoxDecoration(color: nectarine, borderRadius: BorderRadius.all(Radius.circular(10))),
+                                                          child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, crossAxisAlignment: CrossAxisAlignment.start,
+                                                                      children: [Image.asset("assets/icons/meal.png"), Text("Meal plans", style: TextStyle(color: Colors.white, fontFamily: "Futura", fontWeight: FontWeight.w700, fontSize: 18))]))));
+                                                                      // children: [Image.asset("assets/icons/meal.png"), Text("Meal plan", style: TextStyle(color: granite, fontFamily: "Futura", fontWeight: FontWeight.w500, fontSize: 18))]));
 
   }
 
